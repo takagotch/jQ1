928bbd3fa7602460331f68995f25c7f87cd40614
takagotch <dyaccb@gmail.com>
2018-01-10 14:53:27 +0900

    .data('tabid');

--- fr/custom.js
+++ fr/custom.js
@@ -5,17 +5,23 @@ $(function(){
 //  .on('click', 'li > a', function(event){
 	  event.preventDefault();
     var $this = $(this);
-	  $this.parent().siblings()
+//
+	$this.parent().siblings()
 	  .removerClass('selected')
 	  .end()
 	  .addClass('selected');
+//
+	var tabId = $this.data('tabid');
+	$this.closest('.tab').find('.tab-contents').children()
+	.each(function(){
+	var $content = $(this);
+		if($content.data('contentid') == tabId){
+		$content.removeClass('hidden');
+		}else{
+		$contetn.addClass('hidden');
+		}
+	});
   });
 });
 
-$(function(){
-$('tab-menu')
-.on('click', 'li > a', function(event){
-event.prevent
-}){}
-});
 
