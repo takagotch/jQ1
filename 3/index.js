5738d70be1886f7d7173a2d16562bc9d456748bd
takagotch <dyaccb@gmail.com>
2018-01-26 18:25:37 +0900

    p = new P(ctx, px, py); p.push( p ); render();

--- 3/index.js
+++ 3/index.js
@@ -33,46 +33,71 @@ Particle.prototype.initialize = function(x, y){
   };
 
   this.color = {
-    r: Math.floor(),
-    g: Math.floor(),
-    b: Math.floor(),
+    r: Math.floor(Math.random()*255),
+    g: Math.floor(Math.random()*255),
+    b: Math.floor(Math.random()*255),
     a: 1
   };
 }
 
 Particle.prototype.render = function(){
   this.updateParams();
-
-
-
+  this.updatePosition();
+  this.wrapPosition();
+  this.draw();
 }
 
 Particle.prototype.draw = function(){
-
+  ctx = this.ctx;
+  ctx.beginPath();
+  ctx.fillStyle = this.gradient();
+  ctx.arc( this.x, this.y, this.radius, Math.PI*2, false );
+  ctx.fill();
+  ctx.closePath();
 }
 
 Particle.prototype.updateParams = function(){
-
+  var ratio = this.life / this.startLife;
+  this.color.a = 1-ratio;
+  this.radius = 30 / ratio
+  if(this.radius > 300) this.radius = 300;
+  this.life -= 1;
+  if( this.life === 0 ) this.initialize();
 }
 
 Particle.prototype.updatePosition = function(){
-
+  this.x += this.v.x;
+  this.y += this.v.y;
 }
 
 Particle.prototype.wrapPosition = function(){
-
+  if(this.x < 0) this.x = W;
+  if(this.x > W) this.x = 0;
+  if(this.y < 0) this.y = H;
+  if(this.y > H) this.y = 0;
 }
 
 Particle.prototype.gradien = function(){
-
+  var col = this.color.r + ", " + this.color.g + ", " + this.color.b;
+  var g = this.ctx.createRadialGradient( this.x, this.y, 0, this.x, this.y, this.radius);
+  g.addColorStop(0, "rgba(" + col + ", " + (this.color.a * 1) +")");
+  g.addColorStop(0.5, "rgba(" + col + ", " + (this.color.a*0.2) + ")");
+  g.addColorStop(" + col + ", " + (this.color.a * 0) + ")");
+  return g
 }
 
 function render(){
-
+  ctx.clearRect(0,0,W,H);
+  ctx.globalCompositeOperation = "lighter";
+  particles.forEach(function(e){ e.render(); });
+  requestAnimationFrame( render );
 }
 
 for(var i = 0; i < NUM; i++){
-
+  var positionx = Math.random()*W,
+      positiony = Math.random()*H;
+  particle = new Particle(ctx, positionx, positiony);
+  particles.push( particle );
 }
 
 render();
